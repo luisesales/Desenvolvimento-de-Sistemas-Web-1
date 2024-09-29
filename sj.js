c446242cd4e257b46e281e9902d72e4caef6319c
luisesales <duduosales@hotmail.com>
2024-09-29 15:20:23 -0300

    adding final version

--- sj.js
+++ sj.js
@@ -1,21 +1,33 @@
-let calculator = document.getElementsByClassName("calculator");
+
 
 function adicionarValor(valor) {
     document.getElementById('display').value += valor;    
 }
 
 function calcular() {
-    let expressao = document.getElementById('display').value;
-    if (expressao) {
-        try {
-            document.getElementById('display').value = eval(expressao);
-        } catch (e) {
-            alert("Erro na expressão");
+    let coin = Math.floor(Math.random() * 2)
+
+    if(coin){
+        let expressao = document.getElementById('display').value;
+        if (expressao) {
+            try {
+                document.getElementById('display').value = eval(expressao);
+            } catch (e) {
+                alert("Erro na expressão");
+            }
         }
+    }
+    else {
+            let calculator = document.getElementById("body");            
+            
+            let video = document.createElement('div');
+            video.id = 'componente2';
+            video.innerHTML = ' <video class="video" autoplay><source src="Rick Astley - Never Gonna Give You Up (Official Music Video).mp4" type="video/mp4"><source src="movie.ogg" type="video/ogg">Your browser does not support the video tag.</video>';            
+            document.body.replaceChild(video, calculator);
     }
 }
 
 function limparDisplay() {
     document.getElementById('display').value = '';
 }
-
+    
