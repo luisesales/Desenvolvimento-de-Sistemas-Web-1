f5d89692af9cdef27d82bf8987910c50cb25294a
luisesales <duduosales@hotmail.com>
2024-09-29 15:32:11 -0300

    bonus update

--- sj.js
+++ sj.js
@@ -18,10 +18,13 @@ function calcular() {
         }
     }
     else {
+            let header = document.getElementById("header");
+            let title = document.getElementById("title");
             let calculator = document.getElementById("body");            
             
-            let video = document.createElement('div');
-            video.id = 'componente2';
+            let video = document.createElement('div');   
+            title.innerText = 'Never Gonna Give You Up!';
+            header.innerText = 'Já existe um usuário com este cálculo';     
             video.innerHTML = ' <video class="video" autoplay><source src="Rick Astley - Never Gonna Give You Up (Official Music Video).mp4" type="video/mp4"><source src="movie.ogg" type="video/ogg">Your browser does not support the video tag.</video>';            
             document.body.replaceChild(video, calculator);
     }
