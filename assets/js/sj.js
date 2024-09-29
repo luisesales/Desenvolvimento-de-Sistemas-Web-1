612a5046ffb36a8f3df53a1723efe02c0ce6190c
luisesales <duduosales@hotmail.com>
2024-09-29 15:47:57 -0300

    updating to 2.0 version

---
+++ assets/js/sj.js
@@ -0,0 +1,40 @@
+
+
+function adicionarValor(valor) {
+    document.getElementById('display').value += valor;    
+}
+
+function calcular() {
+    let coin = Math.floor(Math.random() * 2)
+
+    if(coin){
+        let expressao = document.getElementById('display').value;
+        if (expressao) {
+            try {
+                document.getElementById('display').value = eval(expressao);
+            } catch (e) {
+                doTheTrick()
+            }
+        }
+    }
+    else {
+        doTheTrick()
+    }
+}
+
+function limparDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function doTheTrick(){
+    let header = document.getElementById("header");
+    let title = document.getElementById("title");
+    let calculator = document.getElementById("body");            
+    
+    let video = document.createElement('div');   
+    title.innerText = 'Never Gonna Give You Up!';
+    header.innerText = 'Já existe um usuário com este cálculo';     
+    video.innerHTML = ' <video class="video" autoplay><source src="./assets/js/test.mp4" type="video/mp4"><source src="movie.ogg" type="video/ogg">Your browser does not support the video tag.</video>';            
+    document.body.replaceChild(video, calculator);
+}
+    
